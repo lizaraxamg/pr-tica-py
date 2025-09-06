ca938a392233169da93217eff3b1ec7d287e77f8
lizaraxamg@proton.me <lizaraxamg@proton.me>
2025-09-06 19:12:26 -0300

    pratica dos conceitos do python

---
+++ pratica.py
@@ -0,0 +1,20 @@
+#print("Eu sou Feliz!")
+
+#print("Eu sou guiada para alcançar a vitória")
+
+x = 12
+y = 20
+
+print(x)
+print(y)
+
+print(x+y)
+
+
+x = str(6)
+y = int(7)
+z = float(8)
+print(x, y, z)
+print(type(x))
+print(type(y))
+print(type(z))
---
+++ prática de python/revisao.py
@@ -0,0 +1,61 @@
+#print("Ola Araxá!!!Bom dia!!!")
+
+#condicional
+
+#if 10 > 8:
+ #   print("A matemática é lógica pura.")
+#else:
+ #   print("A matemática não faz sentido.")   
+    
+#variaveis
+
+#x = 30
+#y = "Joseph"
+
+#print(type(x))
+#print(type(y))  
+
+#x= str(6)
+#y = int(6) 
+#z = float(6) 
+
+#print(x)
+#print(y)
+#print(z)
+
+#shoes = [shoe, sandal, boot]
+#x,y,z = shoes
+#print(x)
+#print(y)
+#print(z)
+
+#fruits = [apple, cherry, banana]
+#x,y,z = fruits
+#print(x)
+#print(y)
+#print(z)
+
+
+
+#x = 20
+#y = 12
+#print(x + y)
+
+#x = "Eu"
+#y = "gosto"
+z# = "de estudar."
+#print(x + y + z)
+
+nome = input("Informe seu nome:")
+idade =  input("Informe sua idade:")
+escolaridade = input("informe sua escolaridade:")
+sexo = input("Informe seu sexo:")
+orientacao_sexual = input("informe sua orientação sexual:")
+print(nome, idade , escolaridade)
+print(nome, idade, sexo)
+print(nome, sexo, orientacao_sexual)
+
+
+
+
+ 
